988c00fccfb4e714115a2398f439c51c4a359c35
Karen <karencinca@hotmail.com>
2023-03-10 21:56:41 -0300

    banner-container

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -22,9 +22,15 @@ function Banner() {
     }, [])
 
     return (
-        <div>
-            <h1>Banner</h1>
-        </div>
+        <header 
+        className="banner-container" 
+        style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            roundPosition: "center-center"
+            }}
+            
+        ></header>
     )
 }
 
